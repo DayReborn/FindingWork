1112fd96599b5034149c2ace18e4499affd0a7b8
zhenxingzhu <zhenxing.zhu@sioux.asia>
2025-03-06 14:35:03 +0800

    ✨ feat(Hot66): 完成力扣Hot66
    完成了力扣Hot100的第66题——搜索旋转排序数组

--- study_notes/leecode/Hot66.cpp
+++ study_notes/leecode/Hot66.cpp
@@ -7,10 +7,56 @@
 
     你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
 
-    
+
 
     示例 1：
 
     输入：nums = [4,5,6,7,0,1,2], target = 0
     输出：4
 */
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int left = 0, right = nums.size();
+        if (target < nums[left] && target > nums[right - 1])
+            return -1;
+        else if (target == nums[left])
+            return 0;
+        else if (target == nums[right - 1])
+            return right - 1;
+        else if (target > nums[left])
+        {
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < nums[left] || target < nums[mid])
+                    right = mid;
+                else if (target > nums[mid])
+                    left = mid + 1;
+                else
+                    return mid;
+            }
+        }
+        else
+        {
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > nums[right - 1] || target > nums[mid])
+                    left = mid + 1;
+                else if (target < nums[mid])
+                    right = mid;
+                else
+                    return mid;
+            }
+        }
+        return -1;
+    }
+};
